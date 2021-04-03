e1478d711a01c3c767fae13e0c4190427307d555
Stephan <stephanrossi@gmail.com>
2021-04-03 09:58:30 -0300

    Arquivos iniciais

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
-function App() {
-  return (
-    <>
-    O
-    </>
-  );
+import HomeScreen from './pages/HomeScreen';
+import Tela2Screen from './pages/Tela2Screen';
+
+export default () => {
+    const name = useSelector(state => state.user.name);
+
+    return (
+        <BrowserRouter>
+            <h1>Oi, {name}</h1>
+
+            <Switch>
+                <Route exact path="/">
+                    <HomeScreen />
+                </Route>
+                <Route path="/tela2/:nome">
+                    <Tela2Screen />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './store';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
